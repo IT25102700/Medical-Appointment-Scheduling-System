8b5986d48f31e8b5b220c176404b2ff1eefa4ba1
nathee <natheesan5832@gmail.com>
2026-05-19 11:19:30 +0530

    initial commit

---
+++ src/main/java/com/medapp/medicalappointmentbookingapp/MedicalAppointmentBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.medapp.medicalappointmentbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentBookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medapp/medicalappointmentbookingapp/MedicalAppointmentBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medapp.medicalappointmentbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
